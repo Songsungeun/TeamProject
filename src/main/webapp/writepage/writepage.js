f66072ce50b39f093a76d07a9123ce8706f005bb
songsungeun <songsungeun@localhost>
2016-10-03 17:41:01 +0900

    글쓰기페이지 - db연동 (회원번호, 카테고리번호, 생성날짜 하드코딩 상태)

--- src/main/webapp/writepage/writepage.js
+++ src/main/webapp/writepage/writepage.js
@@ -7,5 +7,16 @@ $("#submitBoard").click(function(event) {
 	  contents: $("#contents").val(),
 	  url: $("#url").val()
 	}
+	console.log(board)
 	ajaxAddBoard(board)
 });
+
+function ajaxAddBoard(board) {
+	$.post("add.json", board, function(result) {
+		if (result.state != "success") {
+	    	 alert("등록 실패입니다.")
+	    	 return
+	    }
+	    window.location.href = "test.html"
+	}, "json")
+}
