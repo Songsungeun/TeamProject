25b59a934f58ac61036f506b762939c560447c9c
songsungeun <songsungeun@192.168.3.6>
2016-10-04 18:36:33 +0900

    admin 페이지 jquery 적

--- src/main/webapp/adminpage/admin.js
+++ src/main/webapp/adminpage/admin.js
@@ -1,14 +1,6 @@
 function ajaxBoardList() {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState != 4)
-      return;
-    
-    if (xhr.status != 200) {
-      alert("서버에 잘못 요청했습니다.")
-      return;
-    }
-    var result = JSON.parse(xhr.responseText)
+	$.getJSON("adminlist.json")
+  
     
     if (result.state != "success") {
       alert("서버에서 데이터를 가져오는데 실패하였습니다.")
@@ -20,28 +12,18 @@ function ajaxBoardList() {
     for (var i in arr) {
     	  contents += "<tr>" +
           "<td>" + arr[i].no + "</td>" + 
-          "<td>" + arr[i].title + "</td>" +
+          "<td><a class='titleLink' href='#' data-no='" + arr[i].no + "'>" + arr[i].title + "</a></td>" +
           "<td>" + arr[i].createdDate2 + "</td>" +
           "<td>" + arr[i].like + "</td>" + 
           "<td>" + arr[i].viewCount + "</td>" +
           "</tr>"
     }
     
-    document.querySelector("#boardTable tbody").innerHTML = contents;
-  
-    /*  document.querySelector("#tabs-1 ul").innerHTML = contents;
-    var aTags = document.querySelectorAll(".titleLink")
-    for (var i = 0; i < aTags.length; i++) {
-      aTags[i].onclick = function(event) {
-        //alert(this.getAttribute("data-no"))
-        window.location.href = "BoardDetail.html?no=" + this.getAttribute("data-no")
-      }
-    } */
- 
-  }
-  
-  xhr.open("GET", "../admin/list.json", true)
-  xhr.send()
+    $("#boardTable tbody").html = contents;
+    $(".titleLink").click(function(event) {
+    	window.location.href = "../writepage/writepage.html?no=" + $(this).attr("data-no") 
+    })
+   
 }
 
 
