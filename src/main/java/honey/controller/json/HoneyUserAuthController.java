8aa5bb6b204313433bcec8df1ff088477eae79bf
CheolHan <joker10421@gmail.com>
2016-10-04 16:13:31 +0900

    a태그를 이용해서 페이지 이동하던것 javascript를 이용해서 이벤트 발생시 이동으로 변경

--- src/main/java/honey/controller/json/HoneyUserAuthController.java
+++ src/main/java/honey/controller/json/HoneyUserAuthController.java
@@ -21,7 +21,7 @@ import honey.vo.HoneyMembers;
 
 @Controller
 
-@RequestMapping({"/mainpage/", "/writepage/", "/adminpage/"})  // 맵핑정보를 배열형태로 담을수 도 있다!!! 
+@RequestMapping({"/mainpage/", "/writepage/", "/adminpage/","/membership/"})  // 맵핑정보를 배열형태로 담을수 도 있다!!! 
 @SessionAttributes({"HoneyMembers"})  // vo 객체를 맵핑시키나? 뭔지 확실히는 모르겠는데 vo객체 이름 주니까 된다.
 public class HoneyUserAuthController {
 
