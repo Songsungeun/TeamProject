3c27b6ceaf299b53b11c56b0dd535b9b5a6cea91
Mr.Son <Mr.Son@192.168.0.218>
2016-10-04 20:50:15 +0900

    Adminpage 일부 수정

--- src/main/webapp/adminpage/admin.js
+++ src/main/webapp/adminpage/admin.js
@@ -14,6 +14,7 @@ function ajaxBoardList() {
           "<td>" + arr[i].createdDate2 + "</td>" +
           "<td>" + arr[i].like + "</td>" + 
           "<td>" + arr[i].viewCount + "</td>" +
+          "<td>" + "<button type='button' id='boardDelete' class='btn btn-danger'>삭제</button>" + "</td>"
           "</tr>"
     }
     
@@ -21,10 +22,30 @@ function ajaxBoardList() {
     $(".titleLink").click(function(event) {
     	window.location.href = "../writepage/writepage.html?no=" + $(this).attr("data-no") 
     })
+    
+//    $('.btn-danger').confirmAction({
+//  title: {
+//    text: 'Confirm Dialog'
+//  },
+//  message: {
+//    text: 'Are you sure?'
+//  }
+//    });
+    
+    
+    $(".btn-danger").click(function(event){
+    	 var result = confirm("게시물을 삭제하시겠습니까?\n삭제한 게시물은 복구 불가능합니다.");
+         if(result) {
+            //yes
+             location.replace('index.php');
+         } else {
+             //no
+         }
+    });
+    
   })
 }
 
-
 $( function() {
-  $( "#DetailPage_Wrap" ).tabs();
+   $( "#DetailPage_Wrap" ).tabs();
 } );
