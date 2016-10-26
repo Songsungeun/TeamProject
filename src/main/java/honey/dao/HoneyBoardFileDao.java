1714b9092103165ef73c31652ee36883a16f4101
songsungeun <korea.sesong@gmail.com>
2016-10-26 15:55:14 +0900

    .

--- src/main/java/honey/dao/HoneyBoardFileDao.java
+++ src/main/java/honey/dao/HoneyBoardFileDao.java
@@ -5,6 +5,5 @@ import honey.vo.HoneyBoardFile;
 public interface HoneyBoardFileDao {
 
 	int prifileFileinsert (HoneyBoardFile boardFile);
-  List<HoneyBoardFile> getBoardFileName(int boardNo);
   int defaultboardPhotoInsert(int boardNo);
 }
