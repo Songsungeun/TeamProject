8f9c00acc325c287ee2c0a26fc51fb6633904652
Mr.Son <Mr.Son@192.168.3.225>
2016-11-07 18:16:10 +0900

    회원가입페이지 UI Modal 완료

---
+++ src/main/webapp/mainpage/membership.js
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+$("#submitMember").click(function(event) {
+	
+	var board = {
+			userName: $("#name").val(),
+			email: $("#user_Email").val(),
+			nickname: $("#nickname").val(),
+			phone: $("#phone").val(),
+			password: $("#userPassword").val()
+	}
+	ajaxAddMember(board)
+	event.stopImmediatePropagation();
+	}
+	);
+
+$("#unregisterBtn").click(function(event) {
+	var result = confirm("정말탈퇴하시겠습니까?")
+	
+	if (result) {
+	var memberNumber = $("#memberNumber").val();
+	ajaxUnregister(memberNumber);
+	location.href = "../mainpage/Main.html"
+	}
+	else {
+		return
+	}
+	event.stopImmediatePropagation();
+});
+
+function ajaxAddMember(user) {
+	$.ajax({
+		url:serverAddr + "/membership/joinMember.json",
+		type: "POST",
+		dataType: "json",
+		data: user,
+		success: function (obj) {
+			var result = obj.jsonResult
+			if (result.state != "success") {
+				alert("가입실패 하였습니다. 정확히 입력 후 재시도 해주세요")
+				return
+			}
+			alert("축하합니다 가입되었습니다.")
+			window.location.href = "../mainpage/Main.html"
+		},
+		error: function(result) {
+		}
+	})
+}
+
+function ajaxUnregister(memberNo) {
+	$.getJSON(serverAddr + "/mainpage/unregisteMember.json", {
+		memberNo: memberNo
+	}, function(obj) {
+		var result = obj.jsonResult
+		alert("회원탈퇴 성공입니다.")
+		location.href = "../mainpage/Main.html"
+			if (result == "fail") {
+				alert("헤헿 실패 ^^")
+				location.href = "../mainpage/Main.html";
+			}
+	})
+}
