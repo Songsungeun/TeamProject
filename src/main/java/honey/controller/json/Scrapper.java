f87f35dd19465206010a0a0ae7e1b4a26f0cdfaf
songsungeun <korea.sesong@gmail.com>
2016-11-21 11:44:26 +0900

    다음 블로그 및 네이버 블로그 진입점에 따라 스크랩 되도록 수정

--- src/main/java/honey/controller/json/Scrapper.java
+++ src/main/java/honey/controller/json/Scrapper.java
@@ -12,22 +12,22 @@ public class Scrapper {
 
 	public static UrlInfo parsePageHeaderInfo(String urlStr) throws Exception {
 		System.out.println("HTML parsing Start");
-		
+
 		System.out.println("url 연결 시작");
-		
+
 		System.out.println("naver Blog check");
 		String[] checkUrl = urlStr.split("/");
-		
+
 		for (int i = 0; i < checkUrl.length; i++) {
 			System.out.println("checkUrl= " + checkUrl[i]);
 		}
-		
+
 		if (checkUrl[2].equals("blog.naver.com")) {
 			urlStr = checkUrl[0] + "//" + checkUrl[2] + "PostView.nhn?blogId=" + checkUrl[3] + "&logNo=" + checkUrl[4];
 		}
 		Document doc = Jsoup.connect(urlStr).timeout(30000).get();
 		System.out.println("url 소스 긁어옴");
-		
+
 		UrlInfo urlInfo = new UrlInfo();
 
 		String title = null;
@@ -36,7 +36,7 @@ public class Scrapper {
 		if (metaOgTitle != null) {
 			title = metaOgTitle.attr("content");
 		}
-		
+
 		System.out.println("image start");
 		String image = null;
 		Elements metaOgImage = doc.select("meta[property=og:image]");
@@ -54,7 +54,7 @@ public class Scrapper {
 		System.out.println("urlAddr Start");
 		String urlAddr = null;
 		Elements metaOgUrlAddr = doc.select("meta[property=og:url]");
-		
+
 		if (metaOgUrlAddr != null) {
 			String transString = metaOgUrlAddr.attr("content");
 
@@ -79,107 +79,115 @@ public class Scrapper {
 
 	public static UrlInfo UrlForDB(String urlStr) throws Exception {
 		System.out.println("HTML parsing Start");
-		
+
 		System.out.println("url 연결 시작");
 		System.out.println("naver Blog check");
 		String[] checkUrl = urlStr.split("/");
-		
+
 		for (int i = 0; i < checkUrl.length; i++) {
 			System.out.println("checkUrl= " + checkUrl[i]);
 		}
-		
-		if (checkUrl[2].equals("blog.naver.com")) {
+		if (checkUrl[2].equals("blog.naver.com") && (!checkUrl[3].substring(0, 8).equals("PostView"))) {
 			urlStr = checkUrl[0] + "//" + checkUrl[2] + "/PostView.nhn?blogId=" + checkUrl[3] + "&logNo=" + checkUrl[4];
-		}
-		System.out.println("urlStr= " + urlStr);
-		Connection con = Jsoup.connect(urlStr);
-		Document doc = con.timeout(10000).get();
-		UrlInfo urlInfo = new UrlInfo();
-
-		System.out.println("title start");
-		// 제목 파싱 시작!!
-		String title = null;
-		//meta Data에 property로 네이밍한 경우
-		if (!doc.select("meta[property=og:title]").isEmpty()) {
-			//	metaOgTitle = doc.select("meta[property=og:title]");
-			title = doc.select("meta[property=og:title]").attr("content");
-
-			// meta Data에 name으로 네이밍한 경우
-		} else if (!doc.select("meta[name=og:title]").isEmpty()) {
-			title = doc.select("meta[name=og:title]").attr("content");
-
-			// meta Data에 정보 등록 하지 않은 경우
-		} else if (!doc.title().isEmpty()) {
-			title = doc.title();
-		}
-
-		System.out.println("image start");
-		String image = null;
-		Elements metaOgImage = doc.select("meta[property=og:image]");
-		if (!doc.select("meta[property=og:image]").isEmpty()) {
-			image = metaOgImage.attr("content");
-
-		} else if (!doc.select("meta[name=og:image]").isEmpty()) {
-			image = doc.select("meta[name=og:image]").attr("content");
 		} 
-
-		System.out.println("description start");
-		String description = null;
-		//meta Data에 property로 네이밍한 경우
-		if (!doc.select("meta[property=og:description]").isEmpty()) {
-			description = doc.select("meta[property=og:description]").attr("content");
-
-		} else if (!doc.select("meta[name=og:description]").isEmpty()) {
-			description = doc.select("meta[name=og:description]").attr("content");
-		}
 		
-		// description 너무 많이 넣어놓은 나쁜 사이트들 때문에...
-		if (description.length() > 254) {
-			String cutString = description.substring(0, 254);
-			description = cutString;
+			System.out.println("urlStr= " + urlStr);
+			Connection con = Jsoup.connect(urlStr);
+			Document doc = con.timeout(10000).get();
+			UrlInfo urlInfo = new UrlInfo();
+
+			if (checkUrl[2].equals("blog.daum.net") && (!checkUrl[3].substring(0, 5).equals("_blog"))) {
+				System.out.println("frame if 진입");
+				System.out.println("substring= " + checkUrl[3].substring(0, 5));
+				urlStr = "http://blog.daum.net" + doc.select("frame").attr("src");
+				con = Jsoup.connect(urlStr);
+				doc = con.timeout(10000).get();
+			}
+			System.out.println("title start");
+			// 제목 파싱 시작!!
+			String title = null;
+			
+			//meta Data에 property로 네이밍한 경우
+			if (!doc.select("meta[property=og:title]").isEmpty()) {
+				//	metaOgTitle = doc.select("meta[property=og:title]");
+				title = doc.select("meta[property=og:title]").attr("content");
+
+				// meta Data에 name으로 네이밍한 경우
+			} else if (!doc.select("meta[name=og:title]").isEmpty()) {
+				title = doc.select("meta[name=og:title]").attr("content");
+
+				// meta Data에 정보 등록 하지 않은 경우
+			} else if (!doc.title().isEmpty()) {
+				title = doc.title();
+			}
+
+			System.out.println("image start");
+			String image = null;
+			Elements metaOgImage = doc.select("meta[property=og:image]");
+			if (!doc.select("meta[property=og:image]").isEmpty()) {
+				image = metaOgImage.attr("content");
+
+			} else if (!doc.select("meta[name=og:image]").isEmpty()) {
+				image = doc.select("meta[name=og:image]").attr("content");
+			} 
+
+			System.out.println("description start");
+			String description = null;
+			//meta Data에 property로 네이밍한 경우
+			if (!doc.select("meta[property=og:description]").isEmpty()) {
+				description = doc.select("meta[property=og:description]").attr("content");
+
+			} else if (!doc.select("meta[name=og:description]").isEmpty()) {
+				description = doc.select("meta[name=og:description]").attr("content");
+			}
+			System.out.println("description= " + description);
+			// description 너무 많이 넣어놓은 나쁜 사이트들 때문에...
+			if (description.length() > 254) {
+				String cutString = description.substring(0, 254);
+				description = cutString;
+			}
+
+			System.out.println("url Parsing start");
+			// url Parsing 시작!!
+			String urlAddr = null;
+			Elements metaOgUrlAddr = doc.select("meta[property=og:url]");
+
+			if (!doc.select("meta[property=og:url]").isEmpty()) {
+				String transString = doc.select("meta[property=og:url]").attr("content");
+				String temp[]  = transString.split("/");
+
+				urlAddr = temp[2];
+
+				urlAddr = urlAddr.toUpperCase();
+
+			} else if (!doc.select("meta[name=og:url]").isEmpty()){
+				String transString = doc.select("meta[name=og:url]").attr("content");
+				String temp[]  = transString.split("/");
+
+
+				urlAddr = temp[2];
+
+				urlAddr = urlAddr.toUpperCase();
+				System.out.println("어퍼서브: " + urlAddr);
+			} else {
+				System.out.println("og:url 이 읍어");
+			}
+
+			urlInfo.setTitle(title);
+			urlInfo.setImage(image);
+			urlInfo.setDescription(description);
+			urlInfo.setUrlAddr(urlAddr);
+			urlInfo.setDetailUrl(urlStr);
+
+			return urlInfo;
+
+
 		}
-		
-		System.out.println("url Parsing start");
-		// url Parsing 시작!!
-		String urlAddr = null;
-		Elements metaOgUrlAddr = doc.select("meta[property=og:url]");
-
-		if (!doc.select("meta[property=og:url]").isEmpty()) {
-			String transString = doc.select("meta[property=og:url]").attr("content");
-			String temp[]  = transString.split("/");
-
-			urlAddr = temp[2];
-
-			urlAddr = urlAddr.toUpperCase();
-
-		} else if (!doc.select("meta[name=og:url]").isEmpty()){
-			String transString = doc.select("meta[name=og:url]").attr("content");
-			String temp[]  = transString.split("/");
-
-
-			urlAddr = temp[2];
-
-			urlAddr = urlAddr.toUpperCase();
-			System.out.println("어퍼서브: " + urlAddr);
-		} else {
-			System.out.println("og:url 이 읍어");
-		}
-
-		urlInfo.setTitle(title);
-		urlInfo.setImage(image);
-		urlInfo.setDescription(description);
-		urlInfo.setUrlAddr(urlAddr);
-		urlInfo.setDetailUrl(urlStr);
-
-		return urlInfo;
-
-
 	}
-}
 
-//Old 코드
+	//Old 코드
 
-/*StringBuilder sb = new StringBuilder();
+	/*StringBuilder sb = new StringBuilder();
 Connection con = Jsoup.connect(urlStr);
 
  this browseragant thing is important to trick servers into sending us the LARGEST versions of the images 
